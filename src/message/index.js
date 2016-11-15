946fe2be14dfac9404bb169645a40f7ca0233738
haad <haad@headbanggames.com>
2016-11-15 10:21:04 +0100

    Read .proto files as Javascript

--- src/message/index.js
+++ src/message/index.js
@@ -4,12 +4,9 @@ const fs = require('fs')
 const path = require('path')
 const protobuf = require('protocol-buffers')
 
-const rpcSchema = fs.readFileSync(
-    path.join(__dirname, 'rpc.proto'))
-
-const topicDescriptorSchema = fs.readFileSync(
-    path.join(__dirname, 'topic-descriptor.proto'))
+const rpcProto = protobuf(require('./rpc.proto.js'))
+const topicDescriptorProto = protobuf(require('./topic-descriptor.proto.js'))
 
 exports = module.exports
-exports.rpc = protobuf(rpcSchema)
-exports.td = protobuf(topicDescriptorSchema)
+exports.rpc = rpcProto
+exports.td = topicDescriptorProto
--- src/message/topic-descriptor.proto.js
+++ src/message/topic-descriptor.proto.js
@@ -1,3 +1,6 @@
+'use strict'
+
+module.exports = `
 // topicCID = cid(merkledag_protobuf(topicDescriptor)); (not the topic.name)
 message TopicDescriptor {
   optional string name = 1;
@@ -25,4 +28,5 @@ message TopicDescriptor {
       WOT = 2; // web of trust, certificates can allow publisher set to grow
     }
   }
-}
+}`
+
