766745b5fd828b9f788812eca6cd358a4341b04c
Vasco Santos <vasco.santos@moxy.studio>
2020-08-18 18:50:44 +0200

    chore: use refactored base implementation

--- src/index.js
+++ src/index.js
@@ -5,15 +5,10 @@ const debugName = 'libp2p:floodsub'
 const log = debug(debugName)
 log.error = debug(`${debugName}:error`)
 
-const pipe = require('it-pipe')
 const TimeCache = require('time-cache')
-const PeerId = require('peer-id')
 const BaseProtocol = require('libp2p-interfaces/src/pubsub')
-const { message, utils } = require('libp2p-interfaces/src/pubsub')
 
 const { multicodec } = require('./config')
-
-const ensureArray = utils.ensureArray
 
 /**
  * FloodSub (aka dumbsub is an implementation of pubsub focused on
@@ -32,14 +27,9 @@ class FloodSub extends BaseProtocol {
       debugName: debugName,
       multicodecs: multicodec,
       libp2p,
+      canRelayMessage: true,
       ...options
     })
-
-    /**
-     * List of our subscriptions
-     * @type {Set<string>}
-     */
-    this.subscriptions = new Set()
 
     /**
      * Cache of seen messages
@@ -47,310 +37,51 @@ class FloodSub extends BaseProtocol {
      * @type {TimeCache}
      */
     this.seenCache = new TimeCache()
-
-    /**
-     * Pubsub options
-     */
-    this._options = {
-      emitSelf: false,
-      ...options
-    }
-
-    this._processRpc = this._processRpc.bind(this)
   }
 
   /**
-   * Peer connected successfully with pubsub protocol.
+   * Process incoming message
+   * Extends base implementation to check router cache.
    * @override
-   * @param {PeerId} peerId peer id
-   * @param {Connection} conn connection to the peer
+   * @param {InMessage} message The message to process
    * @returns {Promise<void>}
    */
-  async _onPeerConnected (peerId, conn) {
-    await super._onPeerConnected(peerId, conn)
-    const idB58Str = peerId.toB58String()
-
-    // Immediately send my own subscriptions to the newly established conn
-    this._sendSubscriptions(idB58Str, Array.from(this.subscriptions), true)
-  }
-
-  /**
-   * Overriding the implementation of _processConnection should keep the connection and is
-   * responsible for processing each RPC message received by other peers.
-   * @override
-   * @param {string} idB58Str peer id string in base58
-   * @param {DuplexIterableStream} stream inbound stream
-   * @param {PeerStreams} peerStreams PubSub peer
-   * @returns {void}
-   *
-   */
-  async _processMessages (idB58Str, stream, peerStreams) {
-    const onRpcFunc = this._processRpc
-    try {
-      await pipe(
-        stream,
-        async function (source) {
-          for await (const data of source) {
-            const rpc = data instanceof Uint8Array ? data : data.slice()
-
-            onRpcFunc(idB58Str, message.rpc.RPC.decode(rpc))
-          }
-        }
-      )
-    } catch (err) {
-      this._onPeerDisconnected(peerStreams.id, err)
-    }
-  }
-
-  /**
-   * Called for each RPC call received from the given peer
-   * @private
-   * @param {string} idB58Str b58 string PeerId of the connected peer
-   * @param {rpc.RPC} rpc The pubsub RPC message
-   */
-  _processRpc (idB58Str, rpc) {
-    if (!rpc) {
-      return
-    }
-
-    log('rpc from', idB58Str)
-    const subs = rpc.subscriptions
-    const msgs = rpc.msgs
-
-    if (subs && subs.length) {
-      subs.forEach(sub => this._processRpcSubOpt(idB58Str, sub))
-      this.emit('pubsub:subscription-change', PeerId.createFromB58String(idB58Str), subs)
-    }
-
-    if (msgs && msgs.length) {
-      msgs.forEach((msg) => this._processRpcMessage(msg))
-    }
-  }
-
-  /**
-   * Handles an subscription change from a peer
-   *
-   * @param {string} id
-   * @param {RPC.SubOpt} subOpt
-   */
-  _processRpcSubOpt (id, subOpt) {
-    const t = subOpt.topicID
-
-    let topicSet = this.topics.get(t)
-    if (!topicSet) {
-      topicSet = new Set()
-      this.topics.set(t, topicSet)
-    }
-
-    if (subOpt.subscribe) {
-      // subscribe peer to new topic
-      topicSet.add(id)
-    } else {
-      // unsubscribe from existing topic
-      topicSet.delete(id)
-    }
-  }
-
-  /**
-   * @private
-   * @param {rpc.RPC.Message} message The message to process
-   * @returns {void}
-   */
   async _processRpcMessage (message) {
-    const msg = utils.normalizeInRpcMessage(message)
-    const seqno = utils.msgId(msg.from, msg.seqno)
-
-    // 1. check if I've seen the message, if yes, ignore
+    // Check if I've seen the message, if yes, ignore
+    const seqno = this.getMsgId(message)
     if (this.seenCache.has(seqno)) {
       return
     }
-
     this.seenCache.put(seqno)
 
-    // 2. validate the message (signature verification)
-    try {
-      await this.validate(message)
-    } catch (err) {
-      log('Message is not valid, dropping it.', err)
-      return
-    }
-
-    // 3. if message is valid, emit to self
-    this._emitMessages(msg.topicIDs, [msg])
-
-    // 4. if message is valid, propagate msg to others
-    this._forwardMessages(msg.topicIDs, [msg])
+    await super._processRpcMessage(message)
   }
 
-  _emitMessages (topics, messages) {
-    topics.forEach((topic) => {
-      if (!this.subscriptions.has(topic)) {
-        return
-      }
-
-      messages.forEach((message) => {
-        this.emit(topic, message)
-      })
-    })
+  /**
+   * Publish message created. Forward it to the peers.
+   * @override
+   * @param {InMessage} message
+   * @returns {void}
+   */
+  _publish (message) {
+    this._forwardMessage(message)
   }
 
-  _forwardMessages (topics, messages) {
-    topics.forEach((topic) => {
+  /**
+   * Forward message to peers.
+   * @param {InMessage} message
+   * @returns {void}
+   */
+  _forwardMessage (message) {
+    message.topicIDs.forEach((topic) => {
       const peers = this.topics.get(topic)
       if (!peers) {
         return
       }
       peers.forEach((id) => {
-        log('publish msgs on topics', topics, id)
-        this._sendRpc(id, { msgs: messages.map(utils.normalizeOutRpcMessage) })
+        log('publish msgs on topics', message.topicIDs, id)
+        this._sendRpc(id, { msgs: [message] })
       })
-    })
-  }
-
-  /**
-   * Unmounts the floodsub protocol and shuts down every connection
-   * @override
-   * @returns {Promise<void>}
-   */
-  async stop () {
-    await super.stop()
-
-    this.subscriptions = new Set()
-  }
-
-  /**
-   * Publish messages to the given topics.
-   * @override
-   * @param {Array<string>|string} topics
-   * @param {Buffer} message
-   * @returns {Promise<void>}
-   */
-  async publish (topics, message) {
-    if (!this.started) {
-      throw new Error('FloodSub is not started')
-    }
-
-    topics = ensureArray(topics)
-    log('publish', topics, message)
-
-    const from = this.peerId.toB58String()
-    const seqno = utils.randomSeqno()
-    this.seenCache.put(utils.msgId(from, seqno))
-
-    const msgObject = {
-      from,
-      data: message,
-      seqno,
-      topicIDs: topics
-    }
-
-    // Emit to self if I'm interested and it is enabled
-    this._options.emitSelf && this._emitMessages(topics, [msgObject])
-
-    // Normalize message to send
-    const normalizedMessage = await this._buildMessage(msgObject)
-
-    // send to all the other peers
-    this._forwardMessages(topics, [normalizedMessage])
-  }
-
-  /**
-   * Subscribe to the given topic(s).
-   * @override
-   * @param {Array<string>|string} topics
-   * @param {function} [handler]
-   * @returns {void}
-   */
-  subscribe (topics, handler) {
-    if (!this.started) {
-      throw new Error('FloodSub is not started')
-    }
-
-    topics = ensureArray(topics)
-    topics.forEach((topic) => {
-      this.subscriptions.add(topic)
-
-      // Bind provider handler
-      handler && this.on(topic, handler)
-    })
-
-    this.peers.forEach((_, id) => this._sendSubscriptions(id, topics, true))
-  }
-
-  /**
-   * Unsubscribe from the given topic(s).
-   * @override
-   * @param {Array<string>|string} topics
-   * @param {function} [handler]
-   * @returns {void}
-   */
-  unsubscribe (topics, handler) {
-    if (!this.started) {
-      throw new Error('FloodSub is not started')
-    }
-
-    topics = ensureArray(topics)
-
-    topics.forEach((topic) => {
-      this.subscriptions.delete(topic)
-
-      // Remove bind handlers
-      if (!handler) {
-        this.removeAllListeners(topic)
-      } else {
-        this.removeListener(topic, handler)
-      }
-    })
-
-    this.peers.forEach((_, id) => this._sendSubscriptions(id, topics, false))
-  }
-
-  /**
-   * Get the list of topics which the peer is subscribed to.
-   * @override
-   * @returns {Array<String>}
-   */
-  getTopics () {
-    if (!this.started) {
-      throw new Error('FloodSub is not started')
-    }
-
-    return Array.from(this.subscriptions)
-  }
-
-  /**
-   * Encode an rpc object to a buffer
-   * @param {RPC} rpc
-   * @returns {Buffer}
-   */
-  _encodeRpc (rpc) {
-    return message.rpc.RPC.encode(rpc)
-  }
-
-  /**
-   * Send an rpc object to a peer
-   * @param {string} id peer id
-   * @param {RPC} rpc
-   * @returns {void}
-   */
-  _sendRpc (id, rpc) {
-    const peerStreams = this.peers.get(id)
-    if (!peerStreams || !peerStreams.isWritable) {
-      return
-    }
-    peerStreams.write(this._encodeRpc(rpc))
-  }
-
-  /**
-   * Send subscroptions to a peer
-   * @param {string} id peer id
-   * @param {string[]} topics
-   * @param {boolean} subscribe set to false for unsubscriptions
-   * @returns {void}
-   */
-  _sendSubscriptions (id, topics, subscribe) {
-    return this._sendRpc(id, {
-      subscriptions: topics.map(t => ({ topicID: t, subscribe: subscribe }))
     })
   }
 }
--- test/pubsub.spec.js
+++ test/pubsub.spec.js
@@ -6,6 +6,9 @@ const { expect } = require('aegir/utils/chai')
 const sinon = require('sinon')
 const uint8ArrayFromString = require('uint8arrays/from-string')
 
+const PeerId = require('peer-id')
+const PeerStreams = require('libp2p-interfaces/src/pubsub/peer-streams')
+
 const { utils } = require('libp2p-interfaces/src/pubsub')
 const Floodsub = require('../src')
 
@@ -15,6 +18,7 @@ const defOptions = {
   emitSelf: true
 }
 
+// TODO: move to base
 describe('pubsub', () => {
   let floodsub
   let peer
@@ -38,104 +42,105 @@ describe('pubsub', () => {
 
   describe('publish', () => {
     it('should emit non normalized messages', async () => {
-      sinon.spy(floodsub, '_emitMessages')
+      sinon.spy(floodsub, '_emitMessage')
       sinon.spy(utils, 'randomSeqno')
 
       const topic = 'my-topic'
       const message = uint8ArrayFromString('a neat message')
 
       await floodsub.publish(topic, message)
-      expect(floodsub._emitMessages.callCount).to.eql(1)
+      expect(floodsub._emitMessage.callCount).to.eql(1)
 
-      const [topics, messages] = floodsub._emitMessages.getCall(0).args
-      expect(topics).to.eql([topic])
-      expect(messages).to.eql([{
+      const [messageToEmit] = floodsub._emitMessage.getCall(0).args
+      expect(messageToEmit).to.eql({
+        receivedFrom: peer.peerId.toB58String(),
         from: peer.peerId.toB58String(),
         data: message,
         seqno: utils.randomSeqno.getCall(0).returnValue,
-        topicIDs: topics
-      }])
+        topicIDs: [topic]
+      })
     })
 
     it('should forward normalized messages', async () => {
-      sinon.spy(floodsub, '_forwardMessages')
+      sinon.spy(floodsub, '_forwardMessage')
       sinon.spy(utils, 'randomSeqno')
 
       const topic = 'my-topic'
       const message = uint8ArrayFromString('a neat message')
 
       await floodsub.publish(topic, message)
-      expect(floodsub._forwardMessages.callCount).to.eql(1)
-      const [topics, messages] = floodsub._forwardMessages.getCall(0).args
+      expect(floodsub._forwardMessage.callCount).to.eql(1)
+      const [messageToEmit] = floodsub._forwardMessage.getCall(0).args
 
       const expected = await floodsub._buildMessage({
-        from: peer.peerId.toB58String(),
+        receivedFrom: peer.peerId.toB58String(),
+        from: peer.peerId.toBytes(),
         data: message,
         seqno: utils.randomSeqno.getCall(0).returnValue,
-        topicIDs: topics
+        topicIDs: [topic]
       })
 
-      expect(topics).to.eql([topic])
-      expect(messages).to.eql([
-        expected
-      ])
+      expect(messageToEmit).to.eql(expected)
     })
   })
 
   describe('validate', () => {
-    it('should drop unsigned messages', () => {
-      sinon.spy(floodsub, '_emitMessages')
-      sinon.spy(floodsub, '_forwardMessages')
+    it('should drop unsigned messages', async () => {
+      sinon.spy(floodsub, '_emitMessage')
+      sinon.spy(floodsub, '_forwardMessage')
       sinon.spy(floodsub, 'validate')
 
       const topic = 'my-topic'
+      const peerStream = new PeerStreams({ id: await PeerId.create() })
       const rpc = {
         subscriptions: [],
         msgs: [{
-          from: peer.peerId.id,
+          receivedFrom: peerStream.id.toB58String(),
+          from: peerStream.id.toBytes(),
           data: uint8ArrayFromString('an unsigned message'),
           seqno: utils.randomSeqno(),
           topicIDs: [topic]
         }]
       }
 
-      floodsub._processRpc('QmAnotherPeer', rpc)
+      floodsub._processRpc(peerStream.id.toB58String(), peerStream, rpc)
 
       return new Promise((resolve) => {
         setTimeout(() => {
           expect(floodsub.validate.callCount).to.eql(1)
-          expect(floodsub._emitMessages.called).to.eql(false)
-          expect(floodsub._forwardMessages.called).to.eql(false)
+          expect(floodsub._emitMessage.called).to.eql(false)
+          expect(floodsub._forwardMessage.called).to.eql(false)
 
           resolve()
         }, 50)
       })
     })
 
-    it('should not drop unsigned messages if strict signing is disabled', () => {
-      sinon.spy(floodsub, '_emitMessages')
-      sinon.spy(floodsub, '_forwardMessages')
+    it('should not drop unsigned messages if strict signing is disabled', async () => {
+      sinon.spy(floodsub, '_emitMessage')
+      sinon.spy(floodsub, '_forwardMessage')
       sinon.spy(floodsub, 'validate')
       sinon.stub(floodsub, 'strictSigning').value(false)
 
       const topic = 'my-topic'
+      const peerStream = new PeerStreams({ id: await PeerId.create() })
       const rpc = {
         subscriptions: [],
         msgs: [{
-          from: peer.peerId.id,
+          from: peerStream.id.toBytes(),
           data: uint8ArrayFromString('an unsigned message'),
           seqno: utils.randomSeqno(),
           topicIDs: [topic]
         }]
       }
 
-      floodsub._processRpc('QmAnotherPeer', rpc)
+      floodsub._processRpc(peerStream.id.toB58String(), peerStream, rpc)
 
       return new Promise((resolve) => {
         setTimeout(() => {
           expect(floodsub.validate.callCount).to.eql(1)
-          expect(floodsub._emitMessages.called).to.eql(true)
-          expect(floodsub._forwardMessages.called).to.eql(true)
+          expect(floodsub._emitMessage.called).to.eql(true)
+          expect(floodsub._forwardMessage.called).to.eql(true)
 
           resolve()
         }, 50)
